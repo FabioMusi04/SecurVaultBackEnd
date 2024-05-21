9ab9f3322111119dd113564f6f6e098e3e7c3e01
FabioMusi04 <76118020+FabioMusi04@users.noreply.github.com>
2024-05-21 13:59:02 +0200

    fix: update endpoint password

--- Api/password.js
+++ Api/password.js
@@ -14,18 +14,16 @@ const errorHandler = (err, req, res, next) => {
 Router.get('/', verifyToken, async (req, res) => {
     try {
         const user = await User.findById(req.userId).populate('passwords_saved');
-        if(!user) return res.status(404).json({ error: 'User not found' });
-        
+        if (!user) return res.status(404).json({ error: 'User not found' });
+
         const userKey = await deriveKey(user.password, user.salt);
-        if(!userKey) return res.status(500).json({ error: 'Internal Server Error while deriving key' });
-        
+        if (!userKey) return res.status(500).json({ error: 'Internal Server Error while deriving key' });
 
         const passwords = user.passwords_saved.map(item => {
             const decryptedPass = decrypt(item.password, userKey);
             console.log(decryptedPass)
             return { ...item._doc, password: decryptedPass };
         });
-
         res.status(200).json(passwords);
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
@@ -42,10 +40,10 @@ Router.post('/add', verifyToken, async (req, res) => {
         if (email && !validateEmail(email)) return res.status(400).json({ error: 'Invalid email' });
 
         const user = await User.findById(req.userId);
-        if(!user) return res.status(404).json({ error: 'User not found' });
-        
+        if (!user) return res.status(404).json({ error: 'User not found' });
+
         const userKey = await deriveKey(user.password, user.salt);
-        if(!userKey) return res.status(500).json({ error: 'Internal Server Error while deriving key' });
+        if (!userKey) return res.status(500).json({ error: 'Internal Server Error while deriving key' });
 
         const cryptedpass = encrypt(password, userKey);
 
@@ -57,7 +55,7 @@ Router.post('/add', verifyToken, async (req, res) => {
                 password: cryptedpass,
                 icon: icon,
             });
-        } 
+        }
         if (!email && username) {
             newPassword = new PasswordSaved({
                 website: website,
@@ -66,19 +64,19 @@ Router.post('/add', verifyToken, async (req, res) => {
                 icon: icon,
             });
         }
-        if(!newPassword) return res.status(400).json({ error: 'Invalid request' });
+        if (!newPassword) return res.status(400).json({ error: 'Invalid request' });
         const savedPassword = await newPassword.save();
         const updatedUser = await User.findByIdAndUpdate(
-            req.userId, 
+            req.userId,
             { $push: { passwords_saved: savedPassword._id } },
             { new: true }
         );
-        if(!updatedUser) {
+        if (!updatedUser) {
             await PasswordSaved.findOneAndDelete(savedPassword._id);
             return res.status(500).json({ error: 'Internal Server Error while saving on user' });
         }
         savedPassword.password = password
-        res.status(201).json({ message: 'Password saved successfully', password: savedPassword});
+        res.status(201).json({ message: 'Password saved successfully', password: savedPassword });
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
     }
@@ -88,14 +86,30 @@ Router.post('/add', verifyToken, async (req, res) => {
 Router.put('/update/:id', verifyToken, async (req, res) => {
     try {
         const { website, username, email, password } = req.body;
-        const updatedPassword = await PasswordSaved.findByIdAndUpdate(req.params.id, {
-            website,
-            username,
-            email,
-            password,
-            icon,
-            updated_at: Date.now()
-        }, { new: true });
+        if (!email && !username) return res.status(400).json({ error: 'Email or username is required' });
+        if (email && username) return res.status(400).json({ error: 'Email or username can be used' });
+        if (email && !validateEmail(email)) return res.status(400).json({ error: 'Invalid email' });
+        if (!password) return res.status(400).json({ error: 'Password is required' });
+
+        const user = await User.findById(req.userId);
+        if (!user) return res.status(404).json({ error: 'User not found' });
+
+        const userKey = await deriveKey(user.password, user.salt);
+        if (!userKey) return res.status(500).json({ error: 'Internal Server Error while deriving key' });
+
+        const cryptedpass = encrypt(password, userKey);
+
+        let newPassword = null;
+        if (email && !username) {
+            newPassword = { website, email, password: cryptedpass };
+        }
+        if (!email && username) {
+            newPassword = { website, username, password: cryptedpass };
+        }
+        if (!newPassword) return res.status(400).json({ error: 'Invalid request' });
+
+        const updatedPassword = await PasswordSaved.findByIdAndUpdate(req.params.id, newPassword);
+        if (!updatedPassword) return res.status(404).json({ error: 'Password not found' });
         res.status(200).json({ message: 'Password updated successfully', password: updatedPassword });
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
@@ -106,13 +120,13 @@ Router.put('/update/:id', verifyToken, async (req, res) => {
 Router.delete('/remove/:id', verifyToken, async (req, res) => {
     try {
         const removedPassword = await PasswordSaved.findOneAndDelete({ _id: req.params.id });
-        if(!removedPassword) return res.status(404).json({ error: 'Password not found' });
+        if (!removedPassword) return res.status(404).json({ error: 'Password not found' });
         const updatedUser = await User.findByIdAndUpdate(
             req.userId,
             { $pull: { passwords_saved: req.params.id } },
             { new: true }
         );
-        if(!updatedUser){
+        if (!updatedUser) {
             await removedPassword.save();
             return res.status(500).json({ error: 'Internal Server Error while removing on user' });
         }
